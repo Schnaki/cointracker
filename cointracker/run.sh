0dfde26d22847ae9b2b6d56d9759269d6190e9ed
schnaki <schnaki@protonmail.com>
2018-02-15 03:02:13 +0100

    removed some files

--- cointracker/run.sh
+++
@@ -1,1 +0,0 @@
-FLASK_APP=app.py flask run > /dev/null 2>&1 &
--- cointracker/user.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-
-def add_coin(user_id, data):
-    pymongo.update_one({'_id': user_id, {'$addToSet': {'coins': {'name': data['name'], 'amount': data['amount']}}}})
-    
-
